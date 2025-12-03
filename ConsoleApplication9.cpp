457475d55fff6b14726c8883c589a97c9eabf44e
Akeno <holodvada@gmail.com>
2025-12-03 14:59:09 +0700

    My algohomework comment

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm> 
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int* arr, int low, int high) {
+    int pivot = arr[high]; 
+    int i = (low - 1);     
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++; 
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSortRecursive(int* arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSortRecursive(arr, low, pi - 1);
+        quickSortRecursive(arr, pi + 1, high);
+    }
+}
+
+void quick_sort(int* arr, int size) {
+    if (size <= 1) return;
+    quickSortRecursive(arr, 0, size - 1);
+}
+
+void print_array(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << (i == size - 1 ? "" : " ");
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int arr1[] = { 3, 43, 38, 29, 18, 72, 57, 61, 2, 33 };
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+
+    std::cout << "Исходный массив 1:      ";
+    print_array(arr1, size1);
+    quick_sort(arr1, size1);
+    std::cout << "Отсортированный массив 1: ";
+    print_array(arr1, size1);
+    std::cout << std::endl;
+
+    int arr2[] = { 88, 91, 87, 59, 53, 49, 29, 16, 4, 27, 28, 89, 2, 25, 74 };
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    std::cout << "Исходный массив 2:      ";
+    print_array(arr2, size2);
+    quick_sort(arr2, size2);
+    std::cout << "Отсортированный массив 2: ";
+    print_array(arr2, size2);
+    std::cout << std::endl;
+
+    int arr3[] = { 24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
+    int size3 = sizeof(arr3) / sizeof(arr3[0]);
+
+    std::cout << "Исходный массив 3:      ";
+    print_array(arr3, size3);
+    quick_sort(arr3, size3);
+    std::cout << "Отсортированный массив 3: ";
+    print_array(arr3, size3);
+    std::cout << std::endl;
+
+    return 0;
+}
