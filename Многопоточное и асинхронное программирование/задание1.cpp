30bda9d0fdea868a147e22a917ef09ff7c1cc4e8
Akeno <holodvada@gmail.com>
2026-04-21 17:05:46 +0700

    My algohomework comment

---
+++ Многопоточное и асинхронное программирование/задание1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <atomic>
+
+std::atomic<int> client_counter(0);
+
+// Поток клиента
+void client_stream(int max_clients) {
+    for (int i = 0; i < max_clients; ++i) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        client_counter.fetch_add(1);
+        std::cout << "[Клиент] Пришел. Всего в очереди: " << client_counter.load() << std::endl;
+    }
+}
+
+// Поток операциониста
+void operator_stream(int max_clients) {
+    int served_count = 0;
+
+    while (served_count < max_clients) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+
+        if (client_counter.load() > 0) {
+            client_counter.fetch_sub(1);
+            served_count++;
+            std::cout << "[Оператор] Обслужил. Осталось в очереди: " << client_counter.load() << std::endl;
+        }
+        else {
+            std::cout << "[Оператор] Ждет клиентов..." << std::endl;
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    const int MAX_CLIENTS = 5;
+
+    std::cout << "Очередь открыта. Всего ожидается клиентов: " << MAX_CLIENTS << std::endl;
+    std::cout << "-------------------------------------------" << std::endl;
+
+    std::thread t1(client_stream, MAX_CLIENTS);
+    std::thread t2(operator_stream, MAX_CLIENTS);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "-------------------------------------------" << std::endl;
+    std::cout << "Все клиенты обслужены. Очередь пуста." << std::endl;
+
+    return 0;
+}
---
+++ Многопоточное и асинхронное программирование/задание2.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <iomanip>
+#include <string>  
+#include <clocale>
+
+void sum_vectors(const std::vector<int>& v1, const std::vector<int>& v2, std::vector<int>& res, int start, int end) {
+    for (int i = start; i < end; ++i) {
+        res[i] = v1[i] + v2[i];
+    }
+}
+
+double run_test(int num_elements, int num_threads) {
+    std::vector<int> v1(num_elements, 1);
+    std::vector<int> v2(num_elements, 2);
+    std::vector<int> res(num_elements, 0);
+
+    std::vector<std::thread> threads;
+    int chunk_size = num_elements / num_threads;
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < num_threads; ++i) {
+        int start = i * chunk_size;
+        int end = (i == num_threads - 1) ? num_elements : (i + 1) * chunk_size;
+        threads.emplace_back(sum_vectors, std::cref(v1), std::cref(v2), std::ref(res), start, end);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end_time - start_time;
+    return duration.count();
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    unsigned int cores = std::thread::hardware_concurrency();
+    std::cout << "Количество аппаратных ядер: " << cores << "\n\n";
+
+    std::vector<int> sizes = { 1000, 10000, 100000, 1000000 };
+    std::vector<int> thread_counts = { 1, 2, 4, 8, 16 };
+
+    std::cout << std::left << std::setw(15) << "Кол-во эл-тов";
+    for (int t : thread_counts) {
+        std::cout << std::setw(12) << t << " пот.";
+    }
+    std::cout << "\n" << std::string(80, '-') << "\n";
+
+    for (int size : sizes) {
+        std::cout << std::left << std::setw(15) << size;
+        for (int t : thread_counts) {
+            double time = run_test(size, t);
+            std::cout << std::fixed << std::setprecision(5) << std::setw(15) << time;
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
